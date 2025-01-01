c8fa896ce4abcaa6abf35416b24c049e1b35f807
Jack Harvey <53987140+Jack-harvey@users.noreply.github.com>
2025-01-01 11:37:08 +1100

    Add clr, del logic

--- scripts/script.js
+++ scripts/script.js
@@ -22,11 +22,15 @@ function multiplyNumbers(n1, n2) {
 }
 
 function divideNumbers(n1, n2) {
+  if (n1 === 0 || n2 === 0) {
+    updateScreen("err_");
+    return 0;
+  }
   return n1 / n2;
 }
 
-function updateScreen() {
-  screenElement.textContent = workingNumber.join("") + "_";
+function updateScreen(content = workingNumber.join("") + "_") {
+  screenElement.textContent = content;
 
   console.log("workingNumber");
   console.log(workingNumber.join(""));
@@ -43,8 +47,10 @@ function checkWorkingNumber() {
 
 function buildNumber(number) {
   checkWorkingNumber();
+  if (number === "dot" && canDotBePressed === false) return;
   if (number === "dot") {
     workingNumber.push(".");
+    canDotBePressed = false;
     updateScreen();
     return;
   }
@@ -52,10 +58,68 @@ function buildNumber(number) {
   updateScreen();
 }
 
+function calculateMath(symbol) {
+  switch (symbol) {
+    case "add":
+      console.log(symbol);
+      break;
+    case "neg":
+      console.log(symbol);
+      break;
+    case "multi":
+      console.log(symbol);
+      break;
+    case "divide":
+      console.log(symbol);
+      break;
+    case "percent":
+      console.log(symbol);
+      break;
+  }
+}
+
+function symbolPressed(symbol) {
+  if (!signMemory) {
+    signMemory = symbol;
+    return;
+  }
+
+  //equals
+}
+
+function backspace() {
+  let item = workingNumber[workingNumber.length - 1];
+  if (item === ".") {
+    workingNumber.pop();
+    canDotBePressed = true;
+  } else workingNumber.pop();
+  updateScreen();
+  return;
+}
+
+function clear() {
+  numberOneMemory = [];
+  numberTwoMemory = [];
+  signMemory = null;
+  workingNumber = [];
+  buildSecondNumber = false;
+  canDotBePressed = true;
+  updateScreen("_");
+}
+
 function buttonPressed(target) {
   let targetClassList = target.classList;
   if (targetClassList.contains("number")) {
     buildNumber(target.getAttribute("id"));
+  }
+  if (targetClassList.contains("math-btn")) {
+    calculateMath(target.getAttribute("id"));
+  }
+  if (target.getAttribute("id") === "clr") {
+    clear();
+  }
+  if (target.getAttribute("id") === "del") {
+    backspace();
   }
 }
 
